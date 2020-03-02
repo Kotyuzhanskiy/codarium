4caf6c1dc2308ac08177e17898abe640d6e7ffa9
Kotyuzhanskiy <32913672+Kotyuzhanskiy@users.noreply.github.com>
2020-03-02 14:19:45 +0200

    02/03/2020

--- js/my.js
+++ js/my.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
 	$('.header').height($(window).height());
 })
-$(".navbar a").click(function(){
-	$("body,html").animate({
-		scrollTop:$("#" + $(this).data('value')).offset().top
-	},1000)
-})
+
+//  popup menu
+	var navFixed = $("nav.navbar.navbar-expand-lg.navbar-light"),
+    	navHeight = $("nav.navbar.navbar-expand-lg.navbar-light").height();
+
+	if ($(window).scrollTop()) {
+		navFixed.addClass("fixed").removeClass("nofixed");
+	};
+
+	if (navFixed.length > 0) {
+		$(window).scroll(function(){
+			var scroll = $(window).scrollTop();
+            
+            
+			if (scroll < navHeight + 50) {
+				navFixed.addClass("nofixed");
+				navFixed.removeClass("fixed hidden");
+			};
+			if (scroll > navHeight){
+				navFixed.addClass("hidden");
+			};
+			if (scroll > navHeight + 50){
+				navFixed.addClass("fixed");
+				navFixed.removeClass("nofixed");
+			};
+            
+		});
+	};
